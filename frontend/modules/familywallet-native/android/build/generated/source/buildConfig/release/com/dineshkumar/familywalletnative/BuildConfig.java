fd3f293b8314ccae397e73b497e01deac8b677b9
dineshkumarm18031998 <dineshkumarm18031998@gmail.com>
2026-06-13 08:22:04 +0530

    Add .easignore and remove local android cache

---
+++ frontend/modules/familywallet-native/android/build/generated/source/buildConfig/release/com/dineshkumar/familywalletnative/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dineshkumar.familywalletnative;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.dineshkumar.familywalletnative";
+  public static final String BUILD_TYPE = "release";
+}
